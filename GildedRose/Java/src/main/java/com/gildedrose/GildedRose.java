b955e96e8262f9033192f0754d2a5047ed1541ad
sivicako97 <sivicako4@gmail.com>
2019-11-10 17:42:50 +0100

    Version avec les tests corrects. Conjured n'est pas encore codé Une petite partie du Refactoring

--- GildedRose/Java/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,53 +9,59 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (item.name.equals("Aged Brie") || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.name.equals("Aged Brie")) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
                 if (item.quality < 50) {
                     //cas conjured non expire
                     item.quality = item.quality + 1;
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
                         }
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+                    }
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
                         }
                     }
                 }
-            } else {
+            }else {
                 if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    } else {
                         //cas normal non expire
                         item.quality = item.quality - 1;
                     }
                 }
             }
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            } else {
                 item.sellIn = item.sellIn - 1;
             }
             if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                //cas normal expire
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        //cas backstage
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
+                if (item.name.equals("Aged Brie")) {
                     //cas aged  brie
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
                     }
                 } //else conjured
+ else {
+                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        //cas backstage
+                        item.quality = item.quality - item.quality;
+                    } else {
+                        if (item.quality > 0) {
+                            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            } else {
+                                //cas normal expire
+                                item.quality = item.quality - 1;
+                            }
+                        }
+                    }
+                }
             }
         }
     }
