2f26347f08e0def5618aef09ac4ca1db7dffbf2b
sivicako97 <sivicako4@gmail.com>
2019-11-11 15:09:16 +0100

    Version avec les tests corrects. Conjured n'est pas encore codé J'ai enlevé les parties où on avait plusieurs fois les mêmes conditions. Les tests passent.

--- GildedRose/Java/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/Java/src/main/java/com/gildedrose/GildedRose.java
@@ -13,6 +13,12 @@ class GildedRose {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
                 }
+                item.sellIn = item.sellIn - 1;
+                if (item.sellIn < 0) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
             }else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
                 if (item.quality < 50) {
                     //cas conjured non expire
@@ -28,40 +34,21 @@ class GildedRose {
                         }
                     }
                 }
+                item.sellIn = item.sellIn - 1;
+                if (item.sellIn < 0) {
+                    item.quality = 0;
+                }
             }else {
-                if (item.quality > 0) {
-                    if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    } else {
-                        //cas normal non expire
+                if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                } else {
+                    if (item.quality > 0) {
                         item.quality = item.quality - 1;
                     }
-                }
-            }
-            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            } else {
-                item.sellIn = item.sellIn - 1;
-            }
-            if (item.sellIn < 0) {
-                if (item.name.equals("Aged Brie")) {
-                    //cas aged  brie
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
+                    item.sellIn = item.sellIn - 1;
+                    if (item.sellIn < 0 && item.quality > 0) {
+                        item.quality = item.quality - 1;
                     }
-                } //else conjured
- else {
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        //cas backstage
-                        item.quality = item.quality - item.quality;
-                    } else {
-                        if (item.quality > 0) {
-                            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            } else {
-                                //cas normal expire
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    }
-                }
+                } 
             }
         }
     }
